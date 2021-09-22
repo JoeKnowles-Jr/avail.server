4d43a48331401f79b5d02c14a5d720cc6d4ffb5b
JoeKnowles-Jr <89408846+JoeKnowles-Jr@users.noreply.github.com>
2021-09-22 11:07:46 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// hold app secrets and config
-
-module.exports = {
-    "connectionString": "mongodb+srv://availAdmin:rR0IFiqR6wmUJzrp@availcluster.zo4wg.mongodb.net/availdb?retryWrites=true&w=majority",
-    "secret": "airotciv"
-};
